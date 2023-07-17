d603e85b6fa3523d6b39e4c5b33291238ca6e6d6
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-07-17 08:38:45 -0500

    feat: :sparkles: Correciones DB
    Arreglamos la base de datos

--- scripts/db.sql
+++ scripts/db.sql
@@ -1,73 +1,62 @@
 CREATE DATABASE CAMPUSBOARD;
 DROP DATABASE CAMPUSBOARD;
 USE  CAMPUSBOARD;
-CREATE TABLE tareas (
-    id_tarea INT PRIMARY KEY NOT NULL,
-    tarea_asignada VARCHAR(80) NOT NULL,
-    tiempo_inicio DATE NOT NULL,
-    tiempo_entrega DATE NOT NULL
+CREATE TABLE estado(
+    id_estado INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    tipo_estado VARCHAR(30) NOT NULL
 );
-
-CREATE TABLE estado_de_la_tarea (
-    id_estado_tarea INT PRIMARY KEY NOT NULL,
-    estado_tarea VARCHAR(30) NOT NULL
+CREATE TABLE documento(
+    id_documento INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    tipo_documento VARCHAR(30) NOT NULL
 );
-
+CREATE TABLE genero(
+    id_genero INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    tipo_genero VARCHAR(30) NOT NULL
+);
 CREATE TABLE rol (
-    id_rol INT PRIMARY KEY NOT NULL,
+    id_rol INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nombre_rol VARCHAR(20) NOT NULL
 );
-
+CREATE TABLE tareas (
+    id_tarea INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    tarea_asignada VARCHAR(80) NOT NULL,
+    estado_tarea INT NOT NULL,
+    tiempo_inicio DATE NOT NULL,
+    tiempo_entrega DATE NOT NULL,
+    FOREIGN KEY (estado_tarea) REFERENCES estado(id_estado)
+);
+CREATE TABLE proyecto (
+    id_proyecto INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nombre_proyecto VARCHAR(20) NOT NULL,
+    estado_proyecto INT NOT NULL,
+    tiempo_inicio_proyecto DATE NOT NULL,
+    tiempo_entrega_proyecto DATE NOT NULL,
+    FOREIGN KEY (estado_proyecto) REFERENCES estado(id_estado)
+);
 CREATE TABLE usuario (
-    id_usuario INT PRIMARY KEY NOT NULL,
-    rol_usu INT NOT NULL,
-    tarea_usu INT NOT NULL,
-    nombre_usu VARCHAR(20) NOT NULL,
-    segundo_nombre_usu VARCHAR(20) NOT NULL,
-    apellido_usu VARCHAR(20) NOT NULL,
-    segundo_apellido_usu VARCHAR(20) NOT NULL,
-    telefono_usu VARCHAR(15),
-    email_usu VARCHAR(20) NOT NULL,
-    edad_usu INT NOT NULL,
-    tipodoc_usu INT NOT NULL,
-    genero_usu INT NOT NULL,
-    acudiente_usu INT NOT NULL,
-    FOREIGN KEY (rol_usu) REFERENCES rol(id_rol),
-    FOREIGN KEY (tarea_usu) REFERENCES tareas(id_tarea)
+    id_usuario INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    rol_usuario INT NOT NULL,
+    tarea_usuario INT NOT NULL,
+    nombre_usuario VARCHAR(20) NOT NULL,
+    segundo_nombre_usuario VARCHAR(20) NOT NULL,
+    apellido_usuario VARCHAR(20) NOT NULL,
+    segundo_apellido_usuario VARCHAR(20) NOT NULL,
+    telefono_usuario VARCHAR(15),
+    email_usuario VARCHAR(20) NOT NULL,
+    edad_usuario INT NOT NULL,
+    tipodoc_usuario INT NOT NULL,
+    genero_usuario INT NOT NULL,
+    FOREIGN KEY (rol_usuario) REFERENCES rol(id_rol),
+    FOREIGN KEY (tarea_usuario) REFERENCES tareas(id_tarea),
+    FOREIGN KEY (tipodoc_usuario) REFERENCES documento(id_documento),
+    FOREIGN KEY (genero_usuario) REFERENCES genero(id_genero)
 );
 
 CREATE TABLE grupo (
     id_grupo INT PRIMARY KEY NOT NULL,
-    lider_grupo INT NOT NULL,
-    estado_tarea_lider INT NOT NULL,
-    desarrollador_uno INT,
-    estado_tarea_desarrollador_uno INT,
-    desarrollador_dos INT,
-    estado_tarea_desarrollador_dos INT,
-    desarrollador_tres INT,
-    estado_tarea_desarrollador_tres INT,
-    nombre_proyecto INT NOT NULL,
-    fecha_entrega_proyecto DATE NOT NULL,
-    estado_proyecto INT NOT NULL,
-    FOREIGN KEY (lider_grupo) REFERENCES usuario(id_usuario),
-    FOREIGN KEY (estado_tarea_lider) REFERENCES estado_de_la_tarea(id_estado_tarea),
-    FOREIGN KEY (desarrollador_uno) REFERENCES usuario(id_usuario),
-    FOREIGN KEY (estado_tarea_desarrollador_uno) REFERENCES estado_de_la_tarea(id_estado_tarea),
-    FOREIGN KEY (desarrollador_dos) REFERENCES usuario(id_usuario),
-    FOREIGN KEY (estado_tarea_desarrollador_dos) REFERENCES estado_de_la_tarea(id_estado_tarea),
-    FOREIGN KEY (desarrollador_tres) REFERENCES usuario(id_usuario),
-    FOREIGN KEY (estado_tarea_desarrollador_tres) REFERENCES estado_de_la_tarea(id_estado_tarea),
-    FOREIGN KEY (nombre_proyecto) REFERENCES proyecto(id_proyecto),
-    FOREIGN KEY (estado_proyecto) REFERENCES estado_del_proyecto(id_estado_proyecto)
+    desarrollador_grupo INT NOT NULL,
+    proyecto_grupo INT NOT NULL,
+    FOREIGN KEY (desarrollador_grupo) REFERENCES usuario(id_usuario),
+    FOREIGN KEY (proyecto_grupo) REFERENCES proyecto(id_proyecto)
 );
 
-CREATE TABLE proyecto (
-    id_proyecto INT PRIMARY KEY NOT NULL,
-    nombre VARCHAR(20) NOT NULL,
-    tiempo_entrega_proyecto DATE NOT NULL
-);
-
-CREATE TABLE estado_del_proyecto (
-    id_estado_proyecto INT PRIMARY KEY NOT NULL,
-    estado_proyecto VARCHAR(20) NOT NULL
-);
