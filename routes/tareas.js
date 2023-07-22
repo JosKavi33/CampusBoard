03afc677834800039f7fd6ef8c48f782efbc13d5
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-07-22 18:57:14 -0500

    feat: :sparkles: JWT, Cookies metodo POST TB tareas
    Agregamos la seguridad al mertodo post de la tabla tareas mediante jwt y cookies

--- routes/tareas.js
+++ routes/tareas.js
@@ -1,3 +1,4 @@
+import session from 'express-session';
 import mysql from 'mysql2';
 import {Router} from 'express';
 import { SignJWT, jwtVerify } from 'jose'; 
@@ -5,39 +6,47 @@ import proxyTarea from '../middleware/tareasmiddleware.js';
 const storageTarea = Router();
 let con = undefined;
 
+storageTarea.use(session({
+    secret: 'mi-secreto',
+    resave: false, 
+    saveUninitialized: true,   
+}));
+
 storageTarea.use("/:id?", async (req, res, next) => {
-    try {
+    try {  
         const encoder = new TextEncoder();
-        const jwtconstructor = new SignJWT(req.params);
+        const payload = { body: req.body, params: req.params, id: req.params.id  };
+        const jwtconstructor = new SignJWT(payload);
         const jwt = await jwtconstructor 
             .setProtectedHeader({ alg: "HS256", typ: "JWT" })
             .setIssuedAt()
             .setExpirationTime("1h")
-            .sign(encoder.encode(process.env.JWT_PRIVATE_KEY));
-        
-        res.cookie('token', jwt, {httpOnly: true});
-        next();
-    } catch (err) {
+            .sign(encoder.encode(process.env.JWT_PRIVATE_KEY)); 
+        req.body = payload.body;
+        req.session.jwt = jwt;
+        const maxAgeInSeconds = 3600;
+        res.cookie('token', jwt, { httpOnly: true, maxAge: maxAgeInSeconds * 1000 });
+        next();  
+    } catch (err) { 
         console.error('Error al generar el JWT:', err.message);
-        res.sendStatus(500);
+        res.sendStatus(500); 
     }
 });
-
 storageTarea.use((req, res, next) => {
-
     let myConfig = JSON.parse(process.env.MY_CONNECT);
     con = mysql.createPool(myConfig)
     next();
 })
-
 storageTarea.get("/:id?", proxyTarea , async (req,res)=>{
-    const jwt = req.cookies.token; 
-
+    const jwt = req.session.jwt; 
     const encoder = new TextEncoder();  
     const jwtData = await jwtVerify(
         jwt,
         encoder.encode(process.env.JWT_PRIVATE_KEY)
     )
+    if (jwtData.payload.id && jwtData.payload.id !== req.params.id) {
+        return res.sendStatus(403);
+    }
     let sql = (jwtData.payload.id)
         ? [`SELECT id_tarea, tarea_asignada, tiempo_inicio, tiempo_entrega,
         estado.tipo_estado AS estado_tarea
@@ -54,11 +63,11 @@ storageTarea.get("/:id?", proxyTarea , async (req,res)=>{
     );
 })
 
-storageTarea.post("/", proxyTarea ,(req, res) => {
+storageTarea.post("/", proxyTarea ,async (req, res) => {
     con.query(
         /*sql*/
         `INSERT INTO tareas SET ?`,
-        req.body,
+        await getBody(req),
         (err, result) => {
             if (err) {
                 console.error('Error al crear tareas:', err.message);
@@ -101,6 +110,15 @@ storageTarea.delete("/:id",(req, res) => {
         }
     );
 });
-
-
+const getBody = async (req) =>{
+    const jwt = req.session.jwt; 
+    const encoder = new TextEncoder();  
+    const jwtData = await jwtVerify( 
+        jwt,
+        encoder.encode(process.env.JWT_PRIVATE_KEY)
+    );
+    delete jwtData.payload.iat;
+    delete jwtData.payload.exp;   
+    return jwtData.payload.body 
+}
 export default storageTarea;
