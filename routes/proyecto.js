8518b41d280134691c2fbab07adf93618267d013
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-07-22 18:47:39 -0500

    feat: :sparkles: JWT, Cookies metodo POST proyecto
    Implementamos las validaciones para el metodo post de la tabla proyecto

--- routes/proyecto.js
+++ routes/proyecto.js
@@ -1,3 +1,4 @@
+import session from 'express-session';
 import mysql from 'mysql2';
 import {Router} from 'express';
 import { SignJWT, jwtVerify } from 'jose';
@@ -5,39 +6,49 @@ import proxyProyecto from '../middleware/proyectomiddleware.js';
 const storageProyecto = Router();
 let con = undefined;
 
+storageProyecto.use(session({
+    secret: 'mi-secreto',
+    resave: false, 
+    saveUninitialized: true,   
+}));
+
 storageProyecto.use("/:id?", async (req, res, next) => {
-    try {
+    try {  
         const encoder = new TextEncoder();
-        const jwtconstructor = new SignJWT(req.params);
+        const payload = { body: req.body, params: req.params, id: req.params.id  };
+        const jwtconstructor = new SignJWT(payload);
         const jwt = await jwtconstructor 
             .setProtectedHeader({ alg: "HS256", typ: "JWT" })
             .setIssuedAt()
             .setExpirationTime("1h")
-            .sign(encoder.encode(process.env.JWT_PRIVATE_KEY));
-        
-        res.cookie('token', jwt, {httpOnly: true});
-        next();
+            .sign(encoder.encode(process.env.JWT_PRIVATE_KEY)); 
+        req.body = payload.body;
+        req.session.jwt = jwt;
+        const maxAgeInSeconds = 3600;
+        res.cookie('token', jwt, { httpOnly: true, maxAge: maxAgeInSeconds * 1000 });
+        next();  
     } catch (err) { 
         console.error('Error al generar el JWT:', err.message);
-        res.sendStatus(500);
+        res.sendStatus(500); 
     }
 });
 
 storageProyecto.use((req, res, next) => {
-
     let myConfig = JSON.parse(process.env.MY_CONNECT);
     con = mysql.createPool(myConfig)
     next();
 })
 
 storageProyecto.get("/:id?", proxyProyecto, async (req,res)=>{
-    const jwt = req.cookies.token; 
-
+    const jwt = req.session.jwt; 
     const encoder = new TextEncoder();  
     const jwtData = await jwtVerify(
         jwt,
         encoder.encode(process.env.JWT_PRIVATE_KEY)
     )
+    if (jwtData.payload.id && jwtData.payload.id !== req.params.id) {
+        return res.sendStatus(403);
+    }
     let sql = (jwtData.payload.id)
         ? [`SELECT id_proyecto, nombre_proyecto, tiempo_inicio_proyecto, tiempo_entrega_proyecto,
         estado.tipo_estado AS estado_proyecto
@@ -53,12 +64,11 @@ storageProyecto.get("/:id?", proxyProyecto, async (req,res)=>{
         }
     );
 })
-
-storageProyecto.post("/", proxyProyecto ,(req, res) => {
+storageProyecto.post("/", proxyProyecto ,async (req, res) => {
     con.query(
         /*sql*/
         `INSERT INTO proyecto SET ?`,
-        req.body,
+        await getBody(req),
         (err, result) => {
             if (err) {
                 console.error('Error al crear proyecto:', err.message);
@@ -69,8 +79,6 @@ storageProyecto.post("/", proxyProyecto ,(req, res) => {
         }
     );
 });
-
-
 storageProyecto.put("/:id", proxyProyecto ,(req, res) => {
     con.query(
         /*sql*/
@@ -101,6 +109,16 @@ storageProyecto.delete("/:id",(req, res) => {
         }
     );
 });
+const getBody = async (req) =>{
+    const jwt = req.session.jwt; 
+    const encoder = new TextEncoder();  
+    const jwtData = await jwtVerify( 
+        jwt,
+        encoder.encode(process.env.JWT_PRIVATE_KEY)
+    );
 
-
+    delete jwtData.payload.iat;
+    delete jwtData.payload.exp;   
+    return jwtData.payload.body 
+}
 export default storageProyecto;
