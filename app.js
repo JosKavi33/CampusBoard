674f31abc2d11f367f3af2f99c36143d8809ef68
JoseCabrejoVillarCampus <cabre920903@hotmail.com>
2023-07-26 08:31:58 -0500

    feat: :sparkles: Correciones al archivo app.js
    implemeentamos el validate token al app.js, y midlleware token

--- app.js
+++ app.js
@@ -26,19 +26,19 @@ appExpress.get("/token", generateToken, (req,res)=>{
     res.send({token: req.token})
 })
 appExpress.use("/estado", validateToken, storageEstado);
-appExpress.use("/documento", storageDocumento);
-appExpress.use("/genero", storageGenero);
-appExpress.use("/rol", storageRol);
-appExpress.use("/tareas", storageTarea);
-appExpress.use("/proyecto", storageProyecto);
-appExpress.use("/usuario", storageUsuario);
-appExpress.use("/grupo", storageGrupo);
-appExpress.use("/telefono", storageTelefono);
-appExpress.use("/email", storageEmail);
-appExpress.use("/proyecto_usuario", storageProyectoUsuario);
-appExpress.use("/tarea_usuario", storageTareaUsuario);
-appExpress.use("/grupo_usuario", storageGrupoUsuario);
-appExpress.use("/rol_usuario", storageRolUsuario);
+appExpress.use("/documento", validateToken, storageDocumento);
+appExpress.use("/genero", validateToken, storageGenero);
+appExpress.use("/rol", validateToken, storageRol);
+appExpress.use("/tareas", validateToken, storageTarea);
+appExpress.use("/proyecto", validateToken, storageProyecto);
+appExpress.use("/usuario", validateToken, storageUsuario);
+appExpress.use("/grupo", validateToken, storageGrupo);
+appExpress.use("/telefono", validateToken, storageTelefono);
+appExpress.use("/email", validateToken, storageEmail);
+appExpress.use("/proyecto_usuario", validateToken, storageProyectoUsuario);
+appExpress.use("/tarea_usuario", validateToken, storageTareaUsuario);
+appExpress.use("/grupo_usuario", validateToken, storageGrupoUsuario);
+appExpress.use("/rol_usuario", validateToken, storageRolUsuario);
 
 const config =JSON.parse(process.env.MY_CONFIG);
 appExpress.listen(config, ()=>console.log(`http://${config.hostname}:${config.port}`));
